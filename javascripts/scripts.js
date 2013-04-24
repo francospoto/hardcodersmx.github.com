b896165ae348b67ce8190a0cda7252163d29c213
Isaac Zepeda <keogh24@gmail.com>
2013-04-23 18:24:47 -0600

    Site updated at 2013-04-24 00:24:47 UTC

---
+++ javascripts/scripts.js
@@ -0,0 +1,31 @@
+// jQuery("document").ready(function($){
+
+//     var nav = $('ul.main-navigation');
+
+//     $(window).scroll(function () {
+//       console.log('kaka');
+//         if ($(this).scrollTop() > 388) {
+//             nav.addClass("f-nav");
+//         } else {
+//             nav.removeClass("f-nav");
+//         }
+//     });
+
+// });
+
+// jQuery("document").ready(function($){
+
+//     var entry = $('.main-nav-wrap');
+
+//     $(window).scroll(function () {
+//         if ($(this).scrollTop() > 580) {
+//             entry.addClass("remove");
+//             console.log('ya llego');
+//         } else {
+//             entry.removeClass("remove");
+//             console.log('no llego');
+//         }
+//     });
+
+// });
+
